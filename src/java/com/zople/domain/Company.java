ac4c8c92a60b70baa8290ed476f2d06cce53ffd5
wangwy <wangwy@wangwenyan>
2012-08-20 09:28:07 +0800

    初始化365

---
+++ src/java/com/zople/domain/Company.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zople.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+/**
+ *
+ * @author wangwy
+ */
+@Entity
+public class Company implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @SequenceGenerator(name = "company_seq", allocationSize = 1, initialValue = 1, sequenceName = "company_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_seq") 
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Company)) {
+            return false;
+        }
+        Company other = (Company) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.zople.domain.Company[ id=" + id + " ]";
+    }
+    
+}
