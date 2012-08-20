fbff5bfdd4120182fcc1d72056d2cb559ce9e142
wangwy <wangwy@wangwenyan>
2012-08-20 18:15:08 +0800

    初始化365

--- src/java/com/zople/domain/Company.java
+++ src/java/com/zople/domain/Company.java
@@ -5,23 +5,48 @@
 package com.zople.domain;
 
 import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
  * @author wangwy
  */
 @Entity
+@Table(name = "company")
+@NamedQueries({
+    @NamedQuery(name = "Company.findAll", query = "SELECT c FROM Company c")})
 public class Company implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @SequenceGenerator(name = "company_seq", allocationSize = 1, initialValue = 1, sequenceName = "company_seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_seq") 
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
     private Long id;
+    @Size(max = 255)
+    @Column(name = "description")
+    private String description;
+    @Lob
+    @Column(name = "image")
+    private byte[] image;
+    @Size(max = 255)
+    @Column(name = "name")
+    private String name;
+
+    public Company() {
+    }
+
+    public Company(Long id) {
+        this.id = id;
+    }
 
     public Long getId() {
         return id;
@@ -29,6 +54,30 @@ public class Company implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
